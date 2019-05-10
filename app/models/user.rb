a3b79db3fc4c4322c0cf3e4523f70e80b66478a1
TakahikoMurayama0315 <murayama_takahiko@b-engineer.com>
2019-05-10 17:15:20 +0900

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  validates :email, presence: true
+  validates :password, length: { in: 8..32 }
+  validates :password, format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+
+  has_secure_password
 end
